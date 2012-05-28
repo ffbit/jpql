2b43929767fd5f7e0988a1a2a1fef2a51a5fd9e7
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2012-05-28 01:15:13 +0100

    An Employee retrieving test has been written.

---
+++ src/test/java/com/ffbit/jpql/domain/EmployeeJpqlTest.java
@@ -0,0 +1,30 @@
+package com.ffbit.jpql.domain;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.Test;
+
+import com.ffbit.jpql.AbstractPersistenceTest;
+
+public class EmployeeJpqlTest extends AbstractPersistenceTest {
+
+    @Test
+    public void itShouldRetrieveAllEmplloyees() {
+        Employee john = new Employee("John Doe", 1000);
+        Employee jack = new Employee("Jack Line", 1010);
+
+        em.persist(john);
+        em.persist(jack);
+
+        TypedQuery<Employee> query = em.createQuery("SELECT e FROM Employee e",
+                Employee.class);
+        List<Employee> employees = query.getResultList();
+        
+        assertArrayEquals(new Employee[] {john, jack}, employees.toArray());
+    }
+
+}
